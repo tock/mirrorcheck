fa3848e0509e2048b1e7ff35feb55a94179719d7
Leon Schuermann <leon@is.currently.online>
2026-09-23 20:19:04 -0400

    Migrate shell.nix to a flake with a devShell

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "mirrorcheck-shell";
+          packages = [
+            (pkgs.python3.withPackages (ps: with ps; [
+              requests
+              jinja2
+              validators
+            ]))
+          ];
+        };
+      });
+}
